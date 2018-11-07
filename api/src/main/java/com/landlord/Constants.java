d1f7bec51e17fc996cdea7f62a295b463137d7aa
r-paneva <32147137+r-paneva@users.noreply.github.com>
2018-11-07 16:46:58 +0200

    Delete Constants.java

--- api/src/main/java/com/landlord/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.landlord;
-
-public class Constants {
-
-    public static final int USERNAME_VALIDATION_MIN_VALUE = 8;
-    public static final int USERNAME_VALIDATION_MAX_VALUE = 24;
-}
