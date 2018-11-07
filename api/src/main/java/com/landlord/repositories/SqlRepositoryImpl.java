94f58a0105b8de3f54138e09d77159a56b6fcfcc
Rumyana Paneva <r.paneva@gmail.com>
2018-11-07 20:45:56 +0200

    Merge branch 'master' of https://github.com/Merdzhanov/Landlord into springRestApi
    # Conflicts:
    #	api/src/main/java/com/landlord/models/base/ModelBase.java

--- api/src/main/java/com/landlord/repositories/SqlRepositoryImpl.java
+++ api/src/main/java/com/landlord/repositories/SqlRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.landlord.repositories;
 
+import com.landlord.models.ChatMessage;
 import com.landlord.models.Estate;
 import com.landlord.models.User;
 import com.landlord.models.base.ModelBase;
@@ -85,12 +86,12 @@ public class SqlRepositoryImpl implements GenericRepository {
     @Override
     public List<Estate> getEstatesByUser(String userName) {
         List<Estate> result = new ArrayList<>();
-        List<Estate> result2 = new ArrayList<>();
         try (
                 Session session = sessionFactory.openSession();
         ) {
             session.beginTransaction();
-            result = session.createQuery("select E from Estate as E, User as U where U.userName=:userName")
+
+            result = session.createQuery("select E from Estate as E ,User as U where U.userName=:userName and U in elements(E.users)")
                     .setParameter("userName", userName)
                     .list();
             session.getTransaction().commit();
@@ -140,4 +141,37 @@ public class SqlRepositoryImpl implements GenericRepository {
     public void delete(int id) {
 
     }
+
+    @Override
+    public List<ChatMessage> getAllMessages() {
+        List<ChatMessage> result = new ArrayList<>();
+        try (
+                Session session = sessionFactory.openSession();
+        ) {
+            session.beginTransaction();
+            result = session.createQuery("from ChatMessage ").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    @Override
+    public List<ChatMessage> getMessagesByEstate(String name){
+        List<ChatMessage> result = new ArrayList<>();
+        int id;
+        try (
+                Session session = sessionFactory.openSession();
+        ) {
+            session.beginTransaction();
+            result = session.createQuery("select M from ChatMessage as M, Estate as E where E.name = :name and E = M.estate")
+                    .setParameter("name", name)
+                    .list();
+            session.getTransaction().commit();
+        }
+        return result;
+    }
+
 }
